449588eb54571a2c5eeb8afe6d13fee5e6e0e600
Steven Mwesigwa <contact@stevenmwesigwa.com>
2019-11-10 06:05:15 +0300

    Designing logic for Favorites screen in progress

---
+++ src/main/java/com/stevenmwesigwa/musicapp/adapters/FavoriteScreenAdapter.java
@@ -0,0 +1,124 @@
+package com.stevenmwesigwa.musicapp.adapters;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.stevenmwesigwa.musicapp.R;
+import com.stevenmwesigwa.musicapp.Songs;
+import com.stevenmwesigwa.musicapp.fragments.SongPlayingFragment;
+
+import java.util.ArrayList;
+
+public class FavoriteScreenAdapter extends RecyclerView.Adapter<FavoriteScreenAdapter.FavoriteScreenViewHolder>  {
+    /**
+     * Define LayoutInflater
+     */
+    private LayoutInflater mInflater;
+    /**
+     * Define the data
+     */
+    private ArrayList<Songs> mSongsList;
+    private Context mContext;
+
+    // data is passed into the constructor
+    public FavoriteScreenAdapter(ArrayList<Songs> songsList, Context context) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mSongsList = songsList;
+        this.mContext = context;
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+
+    /**
+     * This is where we will initialize our views
+     */
+    public class FavoriteScreenViewHolder extends RecyclerView.ViewHolder {
+        TextView trackTitle;
+        TextView trackArtist;
+        RelativeLayout contentRowSongList;
+
+        public FavoriteScreenViewHolder(View view) {
+            super(view);
+            trackTitle = view.findViewById(R.id.trackTitle);
+            trackArtist = view.findViewById(R.id.trackArtist);
+            contentRowSongList = view.findViewById(R.id.contentRowSongList);
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public FavoriteScreenAdapter.FavoriteScreenViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /* We inflate the xml which gives us a View object */
+        View view = mInflater.inflate(R.layout.row_custom_mainscreen_adapter, parent, false);
+        return new FavoriteScreenAdapter.FavoriteScreenViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoriteScreenAdapter.FavoriteScreenViewHolder holder, int position) {
+        Songs song = mSongsList.get(position);
+
+        holder.trackTitle.setText(song.getSongTitle());
+        holder.trackArtist.setText(song.getSongArtist());
+        /**
+         * Implement the onClickListener on the 'navdrawerItemContentHolder' to open the respective
+         * Fragment on click.
+         */
+        holder.contentRowSongList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final SongPlayingFragment songPlayingFragment = new SongPlayingFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("songArtist", song.getSongArtist());
+                bundle.putString("songTitle", song.getSongTitle());
+                bundle.putLong("songId", song.getSongId());
+                bundle.putString("songData", song.getSongData());
+                bundle.putLong("songDateAdded", song.getSongDateAdded());
+                bundle.putInt("songPosition", position);
+                bundle.putParcelableArrayList("songsList", mSongsList);
+
+//Link values with the songPlayingFragment
+                songPlayingFragment.setArguments(bundle);
+                /**
+                 * Let's begin the transaction
+                 * We will invoke this Adapter through our MainActivity.java file.
+                 */
+                FragmentActivity fragmentActivity = (FragmentActivity) mContext;
+                fragmentActivity.getSupportFragmentManager()
+                        .beginTransaction()
+                        /**
+                         * Replace the the already added fragment from MainActivity.java
+                         */
+                        .replace(R.id.detailsFragment,songPlayingFragment)
+                        .commit();
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+
+        if(mSongsList == null) {
+
+            return 0;
+        } else {
+            return mSongsList.size();
+        }
+
+    }
+
+
+}
+
--- src/main/java/com/stevenmwesigwa/musicapp/fragments/FavoriteFragment.java
+++ src/main/java/com/stevenmwesigwa/musicapp/fragments/FavoriteFragment.java
@@ -1,22 +1,47 @@
 package com.stevenmwesigwa.musicapp.fragments;
 
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.stevenmwesigwa.musicapp.R;
+import com.stevenmwesigwa.musicapp.Songs;
+import com.stevenmwesigwa.musicapp.adapters.FavoriteScreenAdapter;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FavoriteFragment extends Fragment {
-
+private Activity activity = null;
+private ArrayList<Songs> songsArrayList = null;
+private TextView noFavoritesFavFrag = null;
+private RelativeLayout hiddenBottomBarMainScreen = null;
+private TextView songTitleMainScreen = null;
+private ImageButton playPauseButtonMainScreen = null;
+private RecyclerView favoriteRecyclerFavFrag = null;
 
     public FavoriteFragment() {
         // Required empty public constructor
@@ -27,7 +52,169 @@ public class FavoriteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        View view =  inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        noFavoritesFavFrag = view.findViewById(R.id.noFavoritesFavFrag);
+        hiddenBottomBarMainScreen = view.findViewById(R.id.hiddenBottomBarMainScreen);
+        songTitleMainScreen = view.findViewById(R.id.songTitleMainScreen);
+        playPauseButtonMainScreen = view.findViewById(R.id.playPauseButtonMainScreen);
+        favoriteRecyclerFavFrag = view.findViewById(R.id.favoriteRecyclerFavFrag);
+
+
+return view;
     }
 
+    /**
+     * Called when a fragment is first attached to its context.
+     * {@link #onCreate(Bundle)} will be called after this.
+     *
+     * @param context
+     */
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.activity = (Activity) context;
+    }
+
+    /**
+     * Called when a fragment is first attached to its activity.
+     * {@link #onCreate(Bundle)} will be called after this.
+     *
+     * @param activity
+     * @deprecated See {@link #onAttach(Context)}.
+     */
+    @Override
+    public void onAttach(@NonNull Activity activity) {
+        super.onAttach(activity);
+        this.activity = activity;
+    }
+
+    /**
+     * Called to do initial creation of a fragment.  This is called after
+     * {@link #onAttach(Activity)} and before
+     * {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     *
+     * <p>Note that this can be called while the fragment's activity is
+     * still in the process of being created.  As such, you can not rely
+     * on things like the activity's content view hierarchy being initialized
+     * at this point.  If you want to do work once the activity itself is
+     * created, see {@link #onActivityCreated(Bundle)}.
+     *
+     * <p>Any restored child fragments will be created before the base
+     * <code>Fragment.onCreate</code> method returns.</p>
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * Called when the fragment's activity has been created and this
+     * fragment's view hierarchy instantiated.  It can be used to do final
+     * initialization once these pieces are in place, such as retrieving
+     * views or restoring state.  It is also useful for fragments that use
+     * {@link #setRetainInstance(boolean)} to retain their instance,
+     * as this callback tells the fragment when it is fully associated with
+     * the new activity instance.  This is called after {@link #onCreateView}
+     * and before {@link #onViewStateRestored(Bundle)}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        songsArrayList = getSongsFromDevice();
+
+        /*
+        * If 'songsArrayList' is null, make `RecyclerView` disappear
+        * and make 'noFavoritesFavFrag' appear.
+        * Else set up Adapter so that all songs in the database
+        * get displayed on the screen.
+         */
+        if(songsArrayList == null) {
+favoriteRecyclerFavFrag.setVisibility(View.INVISIBLE);
+noFavoritesFavFrag.setVisibility(View.VISIBLE);
+        } else {
+            FavoriteScreenAdapter favoriteScreenAdapter = new FavoriteScreenAdapter(songsArrayList, (Context) activity);
+            /**
+             * Setup LayoutManager - Is responsible for measuring and positioning 'item views' with in a recycler view
+             */
+            // use a linear layout manager
+            RecyclerView.LayoutManager  layoutManager = new LinearLayoutManager( (Context)  activity);
+            favoriteRecyclerFavFrag.setLayoutManager(layoutManager);
+            favoriteRecyclerFavFrag.setItemAnimator(new DefaultItemAnimator());
+            favoriteRecyclerFavFrag.setHasFixedSize(true);
+            /**
+             * Set the adapter to the Recycler View so that the Recycler View will get synced with the
+             * Adapter.
+             */
+            favoriteRecyclerFavFrag.setAdapter(favoriteScreenAdapter);
+        }
+
+    }
+
+    /**
+     * Called when the fragment is visible to the user and actively running.
+     * This is generally
+     * tied to {@link Activity#\onResume() Activity.onResume} of the containing
+     * Activity's lifecycle.
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    /**
+     * Prepare the Fragment host's standard options menu to be displayed.  This is
+     * called right before the menu is shown, every time it is shown.  You can
+     * use this method to efficiently enable/disable items or otherwise
+     * dynamically modify the contents.  See
+     * {@link Activity#onPrepareOptionsMenu(Menu) Activity.onPrepareOptionsMenu}
+     * for more information.
+     *
+     * @param menu The options menu as last shown or first initialized by
+     *             onCreateOptionsMenu().
+     * @see #setHasOptionsMenu
+     * @see #onCreateOptionsMenu
+     */
+    @Override
+    public void onPrepareOptionsMenu(@NonNull Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+    }
+
+    private ArrayList<Songs> getSongsFromDevice() {
+        ArrayList<Songs> songsList = new ArrayList();
+        // Create 'ContentResolver' to access the database
+        ContentResolver contentResolver = activity.getContentResolver();
+        // Create a 'Uri' so that you would fetch a specific song. i.e searching for songs
+        Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        // Make a 'Cursor'
+        // Use the 'ContentResolver' to query the database
+// 'projection' arg is null - because we want to return ALL columns and not a specific one
+        // 'queryArgs' arg is null - because we don't want any arguments inside our query
+        Cursor songCursor = contentResolver.query(songUri, null, null, null);
+//We have to get the columns and data that is fetched only when the cursor is not 'null'
+        if ((songCursor != null) && songCursor.moveToFirst()) {
+            final int id = songCursor.getColumnIndex(MediaStore.Audio.Media._ID);
+            final int title = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            final int artist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            final int data = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+            final int dateAdded = songCursor.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED);
+// Get data inside those columns. Add to the 'songsList'
+            while (songCursor.moveToNext()) {
+                final Long songId = songCursor.getLong(id);
+                final String songTitle = songCursor.getString(title);
+                final String songArtist = songCursor.getString(artist);
+                final String songData = songCursor.getString(data);
+                final Long songDateAdded = songCursor.getLong(dateAdded);
+                songsList.add(new Songs(songId, songTitle, songArtist, songData, songDateAdded));
+            }
+        }
+
+        return songsList;
+    }
 }
