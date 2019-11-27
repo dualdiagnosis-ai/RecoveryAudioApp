790a334b35d2e263d1b6c66b00afd32fbd59b36f
Steven Mwesigwa <contact@stevenmwesigwa.com>
2019-11-27 17:21:38 +0300

    - Rectified AboutUsFragment

--- app/src/main/java/com/stevenmwesigwa/musicapp/fragments/AboutUsFragment.java
+++ app/src/main/java/com/stevenmwesigwa/musicapp/fragments/AboutUsFragment.java
@@ -1,68 +1,31 @@
 package com.stevenmwesigwa.musicapp.fragments;
 
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.stevenmwesigwa.musicapp.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class AboutUsFragment extends Fragment {
-    private Activity activity = null;
-
-
     public AboutUsFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about_us, container, false);
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activity.setTitle("About Us");
-    }
-
-
-    /**
-     * Called when a fragment is first attached to its context.
-     * {@link #onCreate(Bundle)} will be called after this.
-     *
-     * @param context
-     */
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        this.activity = (Activity) context;
-    }
-
-    /**
-     * Called when a fragment is first attached to its activity.
-     * {@link #onCreate(Bundle)} will be called after this.
-     *
-     * @param activity
-     * @deprecated See {@link #onAttach(Context)}.
-     */
-    @Override
-    public void onAttach(@NonNull Activity activity) {
-        super.onAttach(activity);
-        this.activity = activity;
+        getActivity().setTitle("About Us");
     }
 }
